346879da2e10de0f95dcf6781f88b3d43eaa85be
Mateusz Tobiasz <tobiaszmateusz@gmail.com>
2023-01-30 08:31:45 +0100

    Initial commit

---
+++ src/main/java/de/mtobiasz/skyrimd/SkyrimDApplication.java
@@ -0,0 +1,13 @@
+package de.mtobiasz.skyrimd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyrimDApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyrimDApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/mtobiasz/skyrimd/SkyrimDApplicationTests.java
@@ -0,0 +1,13 @@
+package de.mtobiasz.skyrimd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyrimDApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
