e8ec0e1d714a00982fe132f760af12c6f8788ac9
fumo2702 <fumo2702@gmail.com>
2023-02-28 11:24:26 +0100

    Rest tests benötigt Datenbank anbindung und erstellt keine eigenen User

--- src/test/java/de/mtobiasz/skyrimd/RestControllerTest.java
+++ src/test/java/de/mtobiasz/skyrimd/RestControllerTest.java
@@ -20,7 +20,7 @@ class RestControllerTest {
 
     @Test
     void showRandomUser() throws SQLException, JsonProcessingException {
-        var expectedUser = new User(UUID.fromString("22f9d818-9be5-425e-9468-9b16fc7edecb"), "John", null, "Winterhold", LocalDate.parse("12.4.1989"),"test user", "M");
+        var expectedUser = new User(UUID.fromString("22f9d818-9be5-425e-9468-9b16fc7edecb"), "John", null, "Winterhold", LocalDate.of(1999, 2, 12),"test user", "M");
 
         when(DatabaseHandler.getRandomUser()).thenReturn(expectedUser);
 
@@ -54,7 +54,7 @@ class RestControllerTest {
     @Test
     void getUser() throws SQLException {
         var id = UUID.randomUUID();
-        var expectedUser = new User(UUID.fromString("22f9d818-9be5-425e-9468-9b16fc7edecb"), "John", null, "Winterhold", LocalDate.parse("12.4.1989"),"test user", "M");
+        var expectedUser = new User(UUID.fromString("22f9d818-9be5-425e-9468-9b16fc7edecb"), "John", null, "Winterhold", LocalDate.of(1999, 2, 12),"test user", "M");
 
         when(DatabaseHandler.getUserById(id)).thenReturn(expectedUser);
 
@@ -71,8 +71,8 @@ class RestControllerTest {
     @Test
     void getUsers() throws SQLException {
         var expectedUsers = new ArrayList<User>();
-        expectedUsers.add(new User(UUID.fromString("22f9d818-9be5-425e-9468-9b16fc7edecb"), "John", null, "Winterhold", LocalDate.parse("12.4.1989"),"test user", "M"));
-        expectedUsers.add(new User(UUID.fromString("b0d4af09-1671-4f1f-b876-d5120a0fb767"), "Jane", null, "Winterhold", LocalDate.parse("12.4.1989"),"test user", "M"));
+        expectedUsers.add(new User(UUID.fromString("22f9d818-9be5-425e-9468-9b16fc7edecb"), "John", null, "Winterhold", LocalDate.of(1999, 2, 12),"test user", "M"));
+        expectedUsers.add(new User(UUID.fromString("b0d4af09-1671-4f1f-b876-d5120a0fb767"), "Jane", null, "Winterhold", LocalDate.of(1999, 2, 12),"test user", "M"));
 
         when(DatabaseHandler.getUsers()).thenReturn(expectedUsers);
 
@@ -89,10 +89,10 @@ class RestControllerTest {
     void getUsersById() throws SQLException, JsonProcessingException {
         var ids = new ArrayList<UUID>();
         ids.add(UUID.fromString("22f9d818-9be5-425e-9468-9b16fc7edecb"));
-        ids.add(UUID.fromString("2"));
+        ids.add(UUID.fromString("b0d4af09-1671-4f1f-b876-d5120a0fb767"));
         var expectedUsers = new ArrayList<User>();
-        expectedUsers.add(new User(UUID.fromString("22f9d818-9be5-425e-9468-9b16fc7edecb"), "John", null, "Winterhold", LocalDate.parse("12.4.1989"),"test user", "M"));
-        expectedUsers.add(new User(UUID.fromString("b0d4af09-1671-4f1f-b876-d5120a0fb767"), "Jane", null, "Winterhold", LocalDate.parse("12.4.1989"),"test user", "M"));
+        expectedUsers.add(new User(UUID.fromString("22f9d818-9be5-425e-9468-9b16fc7edecb"), "John", null, "Winterhold", LocalDate.of(1999, 2, 12),"test user", "M"));
+        expectedUsers.add(new User(UUID.fromString("b0d4af09-1671-4f1f-b876-d5120a0fb767"), "Jane", null, "Winterhold", LocalDate.of(1999, 2, 12),"test user", "M"));
 
         when(DatabaseHandler.getUsersById(ids)).thenReturn(expectedUsers);
 
